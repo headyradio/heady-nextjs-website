c3e2e5f40daa36bf2a2c4f8b74fd7b833c0d38fd
Johan <johan@Johans-MacBook-Pro-M4.local>
2026-02-25 23:49:03 -0800

    fix: hardcode Sanity projectId fallback for Vercel builds

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET! || "production";
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "jr4q53dp";
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-02-25";
 
 export const client = createClient({
